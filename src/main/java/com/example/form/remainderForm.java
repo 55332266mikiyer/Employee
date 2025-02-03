d84dbbf2d99c6cb0a713c8c4be232cfdd998627e
mikiya.yamada <mikiya.yamada@rakus-partners.co.jp>
2025-02-03 14:18:56 +0900

    First commit

---
+++ src/main/java/com/example/form/remainderForm.java
@@ -0,0 +1,19 @@
+package com.example.form;
+
+public class remainderForm {
+    private String mailAdress;
+
+    public String getMailAdress() {
+        return mailAdress;
+    }
+
+    public void setMailAdress(String mailAdress) {
+        this.mailAdress = mailAdress;
+    }
+
+    @Override
+    public String toString() {
+        return "remainderForm [mailAdress=" + mailAdress + "]";
+    }
+
+}
